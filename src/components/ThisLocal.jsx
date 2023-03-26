eb5a9d9119815dacd5e44f4ab6b8c8d8fbae721a
monoha724 <bunnyalex724@gmail.com>
2023-03-26 22:06:45 +0900

    메인페이지/마이페이지 CSS작업

---
+++ src/components/ThisLocal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ThisLocal() {
+  return (
+    <div>ThisLocal</div>
+  )
+}
+
+export default ThisLocal
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import ProductDetail from "../pages/Home"
-
+import MyPage from "../pages/MyPage"
 
 const Router = () => {
     return (
         <BrowserRouter>
-            <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/detail/:id' element={<ProductDetail />} />
+                <Route path='/mypage' element={<MyPage />} />
             </Routes>
         </BrowserRouter>
     )
